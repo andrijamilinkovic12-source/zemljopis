f4a4d4724592ce24d7d675889d5602bb6cb7d602
Andrija <andrija.milinkovic12@gmail.com>
2026-07-20 21:04:19 +0200

    17

--- www/asset-preloader.js
+++ www/asset-preloader.js
@@ -96,7 +96,7 @@
 
     const TEMATSKI_ASSETI = {
         drzava: [
-            'assets/drzava-static-geo-flags-open-v18.png',
+            'assets/drzava-static-geo-flags-open-v19.png',
             'assets/drzava-static-geo-africa-gold-clean-v3.png',
             'assets/drzava-clouds-without-russia-v1.png'
         ],
